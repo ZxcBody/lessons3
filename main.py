3a97766fcc0b67f8cde16e50451c4900fcb2b000
ZxcBody <aaaawwwaaaqqqwww@gmail.com>
2023-12-23 12:43:27 +0300

    add main

---
+++ main.py
@@ -0,0 +1,10 @@
+class Human:
+    def __init__(self, name = 'nike', height = 170):
+        self.name = name
+        self.height = height
+
+class Bug:
+    def __init__(self, capacity = 25, item = 'book, phone, '):
+        self.capacity = capacity
+        self.item = item
+
